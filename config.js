d73273f2d57604b7b9a7659b9134a3fa8b0792f8
a-p-man <16600886+a-p-man@users.noreply.github.com>
2024-04-30 18:21:35 -0700

    deployment configuration

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = window.location.hostname === 'localhost' ? 'https://localhost:3000' : 'https://ai-taboo-backend.site';
+const API_BASE_URL = 'https://ai-taboo.site';
 
 export default API_BASE_URL;
