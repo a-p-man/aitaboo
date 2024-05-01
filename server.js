d3af8777caf62a96fd5ad2b7ecac2ffaacbf2f47
a-p-man <16600886+a-p-man@users.noreply.github.com>
2024-04-30 18:37:59 -0700

    http config for deploy

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
 const app = express();
 
 app.use(express.static('.'));
 
-const privateKey = fs.readFileSync(path.join(__dirname, 'server.key'), 'utf8');
-const certificate = fs.readFileSync(path.join(__dirname, 'server.cert'), 'utf8');
+const server = http.createServer(app);
 
-const httpsServer = https.createServer({ key: privateKey, cert: certificate }, app);
-
-httpsServer.listen(3000, () => {
-    console.log('Frontend HTTPS server running on https://localhost:3000');
+server.listen(3000, () => {
+    console.log('Frontend server listening on port 3000');
 });
